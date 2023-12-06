46c2a4105183e39f80da32bf5cfb71be059ace30
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-12-06 20:40:32 +0530

    Made Responsive using Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '500px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '620px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '930px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1160px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1300px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
+  
 }
